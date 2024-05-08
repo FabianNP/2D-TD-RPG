174d78435f10daed0017a11429b7947d2608ac33
FabianNP <burner.afire@gmail.com>
2024-05-08 10:27:41 -0600

    Initial commit

---
+++ Assets/Scripts/Managment/Singleton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        /*if (instance == null)
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        */
+
+        if (instance != null && this.gameObject != null)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            instance = this as T;
+        }
+
+        if(!gameObject.transform.parent)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+            
+
+    }
+}
